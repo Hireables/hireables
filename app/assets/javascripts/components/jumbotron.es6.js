e426fb82a2d160f93da1577a1bbc16b07c5e697b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-08 13:55:09 +0100

    Fix wordings for jumbotron

--- app/assets/javascripts/components/jumbotron.es6.js
+++ app/assets/javascripts/components/jumbotron.es6.js
@@ -10,10 +10,10 @@ const Jumbotron = React.createClass({
     return (
         <div className="jumbotron jumbotron--small">
           <div className="col-6">
-            <h3 className="bold">Hire top developers talents through Github</h3>
+            <h3 className="bold">Hire top developers through Github</h3>
             <p className="p-r-60">
-              GithubHire lets you find hireable developers on Github.
-              Just type the company name and sort developers who are hireable.
+              GithubHire lets you find hireable developers on Github.Just type the company
+              name and find developers who are looking to be hired.
             </p>
           </div>
           <div className="organizations-list organizations--small">
