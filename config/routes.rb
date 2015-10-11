23dc29e6df8edd1ca966e3dd08752c7604388f68
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-11 23:06:33 +0100

    Member show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :members, only: :index do
+  resources :members, only: [:index, :show] do
     collection do
       get :popular
       get :search
