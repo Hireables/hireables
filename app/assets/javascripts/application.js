edb2dda19668b256bcdf9198a5771aca2516eecb
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-12 23:09:28 +0100

    Add jquery and jquery_ujs -global

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
 //= require_self
 //= require components
-//= require turbolinks
 //= require react_ujs
 
 // Setup React in global scope
 var React = window.React = global.React = require('react/addons');
-var $ = window.JQuery = require('jquery-browserify');
+window.$ = window.jQuery = require('jquery')
+require('jquery-ujs')
+//= jquery_ujs
