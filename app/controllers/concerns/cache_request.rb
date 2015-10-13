544aca66efd508dff1148fcb7be9484628c90d93
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-13 23:44:12 +0100

    Refactor concerns and move logic to class

--- app/controllers/concerns/cache_request.rb
+++ app/controllers/concerns/cache_request.rb
@@ -1,4 +1,5 @@
 module CacheRequest
+
   # Cache the request cache_key in a redis SET
   extend ActiveSupport::Concern
 
@@ -6,17 +7,27 @@ module CacheRequest
     after_action :cache_request_key, unless: :key_cached?
   end
 
-  def key_cached?
-    # Check if key_cached in redis set?
-    REDIS.sismember("githubhire:cache_keys", cache_key)
-  end
-
   private
+
     def cache_request_key
       # Store the request cache key in a SET
       REDIS.sadd("githubhire:cache_keys", cache_key)
       # Expire the keys in 2 days
       REDIS.expire("githubhire:cache_keys", 2.days)
+    end
+
+    def key_cached?
+      # Check if key_cached in redis set?
+      REDIS.sismember("githubhire:cache_keys", cache_key)
+    end
+
+    # Generate a cache key based on request params
+    def cache_key
+      keys = request_params.map{
+        |key, value| "#{key}:#{value}" unless value.nil?
+      }
+      # Join keys
+      keys.join
     end
 
 end
--- app/controllers/concerns/setup_request_params.rb
+++ app/controllers/concerns/setup_request_params.rb
@@ -7,45 +7,10 @@ module SetupRequestParams
 
   # Setup request param
   def request_params
-    # For home route this will be empty so setup popular params
-    if member_params.empty?
-      popular_params
-    else
-      # Check if keyword is present in query string
-      keyword = member_params["q"].slice!('keyword:')
-      query  = keyword.nil? ? member_params["q"] : format_query(member_params["q"])
-
-      # Format page and query param and add it to request params
-      {
-        page: member_params["page"],
-        q: query
-      }
-    end
-  end
-
-  # Generate a cache key based on request params
-  def cache_key
-    keys = request_params.map{
-      |key, value| "#{key}:#{value}" unless value.nil?
-    }
-    # Join keys
-    keys.join
+    Github::Params.new(member_params).set
   end
 
   private
-
-    def format_query(query)
-      query.slice!('keyword:')
-      query.gsub!(', ', '+')
-    end
-
-    # Hard code popular params, would be good to put in ENV vars
-    def popular_params
-      {
-        followers: ">=1000",
-        repos: ">=20"
-      }
-    end
 
     # Whitelist the params for our controller
     def member_params
