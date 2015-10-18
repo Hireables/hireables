203e9294ddc230d67ca09f060fa2a8939ba11acf
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-18 23:09:19 +0100

    Change style and fonts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,8 @@
 //= require_self
 //= require components
 //= require turbolinks
-//= require react_ujs
 
 // Setup React in global scope
 var React = window.React = global.React = require('react/addons');
 window.$ = window.jQuery = require('jquery')
 require('jquery-ujs')
-//= jquery_ujs
