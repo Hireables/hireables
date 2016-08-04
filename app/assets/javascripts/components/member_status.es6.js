16da5b9a8b821d3e1ffd0613e718b4786439eeb3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-08-04 17:22:17 +0100

    Minor style updates

--- app/assets/javascripts/components/member_status.es6.js
+++ app/assets/javascripts/components/member_status.es6.js
@@ -1,24 +1,29 @@
 // Require React
 React = require('react/addons');
 const createDOMPurify = require('dompurify');
-var classNames = require('classnames');
+const classNames = require('classnames');
 
 // Material UI
 import mui from 'material-ui';
-let Colors = mui.Styles.Colors;
+const Colors = mui.Styles.Colors;
 
 // Define component
 const MemberStatus= React.createClass({
 
   render() {
 
-    let paragraphStyles = {
+    const paragraphStyles = {
       height: 'auto',
       margin: '5px',
       marginLeft: '0px'
     }
 
-    let badgeStyles = {
+    const bioStyles = {
+      fontSize: '14px',
+      maxWidth: '100%',
+    };
+
+    const badgeStyles = {
       fontSize: '11px',
       lineHeight: '22px',
       padding: '5px 8px',
@@ -29,20 +34,21 @@ const MemberStatus= React.createClass({
       borderRadius: 2
     }
 
-    let emailStyles = {
+    const emailStyles = {
       backgroundColor: Colors.yellow900,
       cursor: 'pointer'
     }
 
-    const bio = createDOMPurify.sanitize(this.props.member.bio)
+    const bio = createDOMPurify.sanitize(this.props.member.bio);
 
     return (
         <p style={paragraphStyles}>
           <span style={{color: Colors.darkBlack}}>
             <small>{this.props.member.location}</small>
           </span><br/>
-          {this.props.member.bio? <p dangerouslySetInnerHTML={{__html:bio }}>
-          </p> : ""}
+
+          {this.props.member.bio? <p style={bioStyles} dangerouslySetInnerHTML={{ __html:bio }}></p> : ""}
+
           <div style={{marginTop: '5px'}}>
             {this.props.member.email ? <span onClick={this._openMail} style={$.extend({}, badgeStyles, emailStyles)}>
               Email
