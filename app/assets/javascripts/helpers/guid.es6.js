55e182773cbddcec2b8b0365262fe480655869f8
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-09-06 12:52:20 +0100

    Add guid generator helper

---
+++ app/assets/javascripts/helpers/guid.es6.js
@@ -0,0 +1,14 @@
+// Generate random guid
+
+const s4 = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
+const guid = () => {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
+
+export default guid;
