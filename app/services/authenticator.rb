411e3b2dc7a58b4ea70845405aba5a0a31ac9ef5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-09-13 12:37:29 +0100

    Add more fields

--- app/services/authenticator.rb
+++ app/services/authenticator.rb
@@ -8,8 +8,8 @@ class Authenticator
   def initialize(auth)
     @auth = auth
     @developer = Developer.where(
-      uid: auth.info.uid,
-      provider: auth.info.provider
+      uid: auth.uid,
+      provider: auth.provider
     ).first
   end
 
@@ -24,14 +24,18 @@ class Authenticator
   private
 
   def create_from_omniauth
+    puts auth.extra.raw_info
     Developer.create!(
       email: auth.info.email,
       password: Devise.friendly_token[0, 20],
-      first_name: auth.info.first_name,
-      last_name: auth.info.last_name,
+      name: auth.info.name,
       uid: auth.uid,
+      login: auth.info.nickname,
+      city: auth.extra.raw_info.location,
       provider: auth.provider,
+      available: (auth.extra.raw_info.hireable.nil? ? false : true),
       access_token: auth.credentials.token,
+      data: auth.extra.raw_info
     )
   end
 end
