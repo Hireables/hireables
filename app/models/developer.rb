9537f05d811b1d0e3ef1a43cad743d0fd804879c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-09-13 12:37:39 +0100

    Setup safelisted attributes for api

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,4 +2,20 @@ class Developer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable, :omniauthable
+
+
+  def self.whitelisted_attributes
+    all_attributes - protected_attributes
+  end
+
+  private
+
+  def self.all_attributes
+    self.columns.map(&:name).map(&:to_sym)
+  end
+
+  def self.protected_attributes
+    [:access_token, :provider, :uid, :email]
+  end
+
 end
