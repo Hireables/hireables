19e7ba6397736454247f52784553e0518d751fe6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-08 17:08:35 +0100

    Cleanup controllers and concerns

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,76 +1,30 @@
 class DevelopersController < ApplicationController
-  # Common controller concerns
-  include SetupRequestParams
-  include CacheRequest
-  include GetApiUri
+  include RequestHelpers
 
   # GET /developers
   def index
-    # Unless request cached fetch developers async
-    FetchDevelopersJob.perform_later(cache_key, request_uri, params
+    FetchDevelopersJob.perform_later(
+      cache_key,
+      github_api_uri
     ) unless key_cached?
-
-    respond_to do |format|
-      format.html
-    end
   end
 
-  # GET /developers/:username.json
   # GET /developers/:username
   def show
-    # Fetch from cache
-    @developer = Rails.cache.fetch(['developers', params[:id]], expires_in: 2.days) do
-      request = Github::Api.new("/users/#{params[:id]}").fetch
-      if Github::Response.new(request).found?
-        # Fetch user languages
-        FetchDeveloperLanguagesJob.perform_later(params[:id])
-        request.parsed_response
-      else
-        # Raise not found
-        raise ActionController::RoutingError.new('Not Found')
-      end
-    end
-
-    # Fetch languages
-    @languages = Rails.cache.fetch(['developers', params[:id], 'languages'], expires_in: 2.days) do
-      request = Github::Api.new("/users/#{params[:id]}/repos").fetch
-      Github::Response.new(request.parsed_response).developer_languages_collection
-    end
-
-    respond_to do |format|
-      format.html
-      format.json {render json:  {developer: @developer, languages: @languages} }
-    end
+    FetchDeveloperJob.perform_later(params[:id])
+    FetchDeveloperLanguagesJob.perform_later(params[:id])
   end
 
-  # GET /developers/search.json
+  # POST /developers/search.json
   def search
-    # Load developers based on request params
-    response = Rails.cache.fetch(cache_key, expires_in: 2.days) do
-      request = Github::Api.new(request_uri).fetch
-      response = Github::Response.new(request)
-
-      if response.found?
-
-        developers = if params[:hireable]
-                      response.hireable_collection
-                     else
-                      response.developers_collection
-                     end
-
-        # Cache formatted response
-        {
-          developers: developers,
-          rels: Pagination.new(request.headers).build
-        }.to_json
-      else
-        # Raise not found
-        raise ActionController::RoutingError.new('Not Found')
-      end
+    FetchDevelopersJob.perform_later(
+      cache_key,
+      github_api_uri
+    ) unless key_cached?
     end
 
     respond_to do |format|
-      format.json {render json:  response}
+      format.json { head :ok }
     end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,11 @@
 class PagesController < ApplicationController
-  # Setup params for the request
-  include SetupRequestParams
-  include CacheRequest
-  include GetApiUri
+  include RequestHelpers
 
+  # GET /
   def home
-    # Unless request cached fetch async developers
-    FetchDevelopersJob.perform_later(cache_key, request_uri, params
+    FetchDevelopersJob.perform_later(
+      cache_key,
+      request_uri
     ) unless key_cached?
-
-    respond_to do |format|
-      format.html
-    end
   end
 end
