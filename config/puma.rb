ecbed0f8c60e954d90c2a8ead34bbd12208a4122
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-09 21:09:31 -1100

    Add puma

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads threads_count, threads_count
+port ENV.fetch('PORT') { 3000 }
+
+environment ENV.fetch('RAILS_ENV') { 'development' }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+
+preload_app!
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+plugin :tmp_restart
