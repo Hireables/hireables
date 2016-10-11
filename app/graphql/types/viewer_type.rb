7d21cbe175f92c7afe6631e0e9e8414edaf12bd1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-11 12:14:04 +0100

    Add new fields for pagination

--- app/graphql/types/viewer_type.rb
+++ app/graphql/types/viewer_type.rb
@@ -6,6 +6,16 @@ ViewerType = GraphQL::ObjectType.define do
   # `id` exposes the UUID
   global_id_field :id
 
+  field :total_count, types.Int do
+    description 'Total number of results'
+    resolve -> (obj, args, ctx) { DevelopersResolver.new(obj, args, ctx).results }
+  end
+
+  field :pages, types.Int do
+    description 'Total number of pages'
+    resolve -> (obj, args, ctx) { DevelopersResolver.new(obj, args, ctx).pages }
+  end
+
   connection :developers, DeveloperType.connection_type do
     argument :fullname, types.String, default_value: nil
     argument :location, types.String, default_value: nil
@@ -20,6 +30,6 @@ ViewerType = GraphQL::ObjectType.define do
     # argument :parttime, types.Boolean, default_value: nil
     argument :order, types.String, default_value: nil
     description 'Developer connection to fetch paginated developers collection.'
-    resolve(DevelopersResolver)
+    resolve -> (obj, args, ctx) { DevelopersResolver.new(obj, args, ctx).collection }
   end
 end
