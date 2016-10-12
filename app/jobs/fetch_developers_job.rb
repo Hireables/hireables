4eec8e4d2ab0184f40c94c597a6406f4a31db02e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-12 04:54:05 +0100

    Fix the guard clause

--- app/jobs/fetch_developers_job.rb
+++ app/jobs/fetch_developers_job.rb
@@ -5,9 +5,10 @@ class FetchDevelopersJob < ActiveJob::Base
 
   def perform(cache_key)
     search = Rails.cache.read(cache_key)
-
     Rails.cache.fetch([cache_key, 'collection'], expires_in: 2.days) do
-      response.developers_collection
-    end unless search.nil? && search.logins.any?
+      search.logins.map do |username|
+        Developer.fetch_by_login(username)
+      end
+    end unless search.nil?
   end
 end
