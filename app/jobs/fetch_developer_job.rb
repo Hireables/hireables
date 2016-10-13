3f57c08e7b3f3292807ad9e07bda912e5103e2e1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-13 07:06:38 +0100

    Refactor jobs to use updated github api

--- app/jobs/fetch_developer_job.rb
+++
@@ -1,8 +0,0 @@
-class FetchDeveloperJob < ApplicationJob
-  queue_as :urgent
-  rescue_from ActiveRecord::RecordNotFound, &:message
-
-  def perform(username)
-    Developer.fetch_by_login(username)
-  end
-end
--- app/jobs/fetch_developer_languages_job.rb
+++
@@ -1,7 +0,0 @@
-class FetchDeveloperLanguagesJob < ApplicationJob
-  queue_as :urgent
-
-  def perform(username)
-    Developer.fetch_languages_from_github(username)
-  end
-end
