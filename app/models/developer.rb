eef381b5f7e743e976c6649ba0bbc55cafa4982f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-13 11:11:44 +0100

    Cleanup store accessors

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,7 +1,7 @@
 class Developer < ApplicationRecord
   devise :database_authenticatable, :trackable, :validatable, :omniauthable
-  store_accessor :data, :bio, :html_url, :avatar_url, :company,
-  :blog, :location, :followers, :public_gists, :public_repos, :hireable
+  store_accessor :data, :html_url, :avatar_url, :company, :blog,
+  :followers, :public_gists, :public_repos
 
   after_commit :cache_login!
 
