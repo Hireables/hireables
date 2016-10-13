7dd10304b685ba3dd6808bda5e7f35942ef82dac
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-13 07:07:10 +0100

    Setup action cable urls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.action_cable.url = ENV['WEBSOCKET_URL']
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
