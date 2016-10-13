f69609186e0c8522f48822db2465a5fdb61a8e21
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-13 17:01:42 +0100

    Add callbacks to invalidate cache and set premium

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,12 +2,32 @@ class Developer < ApplicationRecord
   devise :database_authenticatable, :trackable, :validatable, :omniauthable
   store_accessor :data, :html_url, :avatar_url, :company, :blog,
   :followers, :public_gists, :public_repos
+  after_commit :cache_login!, :delete_cache!, :delete_languages_cache!
+  after_commit :set_premium!, on: :update, if: :completed?
 
-  after_commit :cache_login!
+  def completed?
+    premium_fields.all?{|field| send(field).present? }
+  end
 
   private
 
   def cache_login!
     REDIS.sadd('hireables:developers_logins', login)
+  end
+
+  def set_premium!
+    update!(premium: true)
+  end
+
+  def delete_cache!
+    Rails.cache.delete(login)
+  end
+
+  def delete_languages_cache!
+    Rails.cache.delete([login, 'languages'])
+  end
+
+  def premium_fields
+    %w(bio email platforms location)
   end
 end
