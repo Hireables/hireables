1814ab8ba518f290a63b2afd3738e7191f37ee2b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-14 15:59:34 +0100

    Cleanup authentication token helpers

--- app/assets/javascripts/helpers/authentication.es6.js
+++ app/assets/javascripts/helpers/authentication.es6.js
@@ -3,14 +3,4 @@ export default class Authentication {
     const csrfNode = document.getElementsByName('csrf-token')[0];
     return csrfNode ? csrfNode.content : '';
   }
-
-  authToken() {
-    const tokenNode = document.getElementsByName('auth-token')[0];
-    return tokenNode ? tokenNode.content : '';
-  }
-
-  wistiaToken() {
-    const tokenNode = document.getElementsByName('wistia-token')[0];
-    return tokenNode ? tokenNode.content : '';
-  }
 }
