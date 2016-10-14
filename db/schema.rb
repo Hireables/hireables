6cfc401386fd36f3d9ca0604bf15c495989442ce
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-14 17:25:48 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912161658) do
+ActiveRecord::Schema.define(version: 20161014150901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,36 @@ ActiveRecord::Schema.define(version: 20160912161658) do
     t.index ["relocate"], name: "index_developers_on_relocate", using: :btree
     t.index ["remote"], name: "index_developers_on_remote", using: :btree
     t.index ["subscribed"], name: "index_developers_on_subscribed", using: :btree
+  end
+
+  create_table "recruiters", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "name",                   default: "",    null: false
+    t.boolean  "verified",               default: false
+    t.string   "company",                default: "",    null: false
+    t.string   "website",                default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,     null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.index ["confirmation_token"], name: "index_recruiters_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_recruiters_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_recruiters_on_unlock_token", unique: true, using: :btree
   end
 
 end
