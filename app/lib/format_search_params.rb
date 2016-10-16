fcfc0395563569bb34c99f0f86976da91346b31c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-16 05:05:00 +0100

    Refactor param formatting class

--- app/lib/format_search_params.rb
+++ app/lib/format_search_params.rb
@@ -6,7 +6,7 @@ class FormatSearchParams
   end
 
   def to_query
-    request_params.map do |param, value|
+    supported_params.map do |param, value|
       "#{param}:#{value}"
     end.compact.join(' ') << " #{params['fullname']}"
   end
@@ -15,27 +15,23 @@ class FormatSearchParams
     to_query.gsub(/\s+/, '') << "&page=#{params['page'] || 1}"
   end
 
-  def request_params
-    valid_params.any? ? valid_params : default_params
+  def supported_params
+    params.select do |param, value|
+      supported?(param, value)
+    end
   end
 
-  def valid_params
-    params.to_h.select do |param, value|
-      valid?(param, value) && value.present?
-    end
+  def valid?
+    supported_params.any?
   end
 
   private
 
-  def valid?(key, value)
+  def supported?(key, value)
     supported.include?(key.to_s) && value.present?
   end
 
   def supported
     %w(language location followers repos)
-  end
-
-  def default_params
-    { followers: '>10', repos: '>10' }
   end
 end
