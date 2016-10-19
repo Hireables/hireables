90fea925f216261cfadacb91987b1b4f30f27554
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-19 08:52:42 +0100

    Change to recruiter

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,17 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     include AuthToken
-    identified_by :current_developer
+    identified_by :current_recruiter
 
     def connect
       reject_unauthorized_connection && return unless valid_token?
-      self.current_developer = find_current_developer
+      self.current_recruiter = find_current_recruiter
     end
 
     protected
 
-    def find_current_developer
-      Developer.find_by(id: cookies.signed['developer.id'])
+    def find_current_recruiter
+      Recruiter.find_by(id: cookies.signed['recruiter.id'])
     end
   end
 end
