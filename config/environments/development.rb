a49c92ddab346fb39d14b47ee0224a90122eb084
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-19 09:05:07 +0100

    Remove reference to action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  config.action_cable.url = ENV['WEBSOCKET_URL']
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_cable.url = ENV['WEBSOCKET_URL']
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
