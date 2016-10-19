5deb4ae5b835a07dac709e88daad92403045666f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-19 10:12:33 +0100

    Add mailer options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # config.action_cable.url = ENV['WEBSOCKET_URL']
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
