fd56f8d834f8a43e3d36ba410339edd6f8a4d008
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-20 07:42:37 +0100

    Add store accessor for preferences and expire search query if preferences update

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,5 +1,14 @@
 class Recruiter < ApplicationRecord
+  store :preferences, accessors: [ :language, :location ], coder: JSON
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable
+
+  after_commit :expire_search_query, if: :preferences_previously_changed?
+
+  private
+
+  def expire_search_query
+    Rails.cache.delete('search_query')
+  end
 end
