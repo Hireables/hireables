1b48c3b63d9e45cc2bba6ca40371da0dcd61fbc4
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-21 11:14:02 +0100

    Fix fields

--- db/migrate/20161014150901_devise_create_recruiters.rb
+++ db/migrate/20161014150901_devise_create_recruiters.rb
@@ -27,23 +27,10 @@ class DeviseCreateRecruiters < ActiveRecord::Migration[5.0]
       t.inet     :current_sign_in_ip
       t.inet     :last_sign_in_ip
 
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      t.string   :unlock_token # Only if unlock strategy is :email or :both
-      t.datetime :locked_at
-
       t.timestamps null: false
     end
 
     add_index :recruiters, :email,                unique: true
     add_index :recruiters, :reset_password_token, unique: true
-    add_index :recruiters, :confirmation_token,   unique: true
-    add_index :recruiters, :unlock_token,         unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -74,19 +74,10 @@ ActiveRecord::Schema.define(version: 20161014150901) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,     null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.index ["confirmation_token"], name: "index_recruiters_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_recruiters_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true, using: :btree
-    t.index ["unlock_token"], name: "index_recruiters_on_unlock_token", unique: true, using: :btree
   end
 
 end
