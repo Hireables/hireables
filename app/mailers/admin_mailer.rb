5e9a7807bb344b515f786d0c9c7a3f74c3c0a07a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-22 09:47:38 +0100

    Setup default to and from for admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,10 @@
 class AdminMailer < ActionMailer::Base
+  default to: ENV.fetch('SIGNUPS_EMAIL'),
+          from: ENV.fetch('MAILER_EMAIL')
+
   def new_recruiter_signup(recruiter_id)
     @recruiter = Recruiter.find(recruiter_id)
-    mail(from: @recruiter.email, to: 'parul.rhl@gmail.com', subject: 'New Recruiter signup!') do |format|
+    mail(subject: 'New Recruiter signup!') do |format|
       format.text
       format.html
     end
