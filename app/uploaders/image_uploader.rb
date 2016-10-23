13189f205394230485e93d8c11a24284faeffe62
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-23 11:09:58 +0100

    Add carrierwave config and uploader

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,19 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :square do
+    process resize_to_fill: [200, 113]
+  end
+
+  version :thumb do
+    process resize_to_fill: [200, 200]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+end
