56e758da33073a2a86647c5a34ef6e6bc2959523
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-25 16:39:03 +0100

    Authenticate developer for editing profile

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,5 @@
 class DevelopersController < ApplicationController
-  before_action :authenticate_developer!, only: :profile
+  before_action :authenticate_developer!, only: [:profile, :edit]
   before_action :set_developer, only: [:show, :edit]
   before_action :authenticate_recruiter!, only: :show
 
