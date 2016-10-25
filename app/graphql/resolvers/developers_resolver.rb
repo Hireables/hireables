1c2f10cdf4cbe9f037a2ad8668405a56a9fb4f61
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-25 11:06:21 +0100

    Reduce the page size

--- app/graphql/resolvers/developers_resolver.rb
+++ app/graphql/resolvers/developers_resolver.rb
@@ -15,7 +15,7 @@ class DevelopersResolver
       hireables = api.fetch_hireable_developers(query)
       count = hireables.length
 
-      while count < 51 && query[:page] < 10
+      while count < 51 && query[:page] < 5
         query[:page] += 1
         hireables.push(*(api.fetch_hireable_developers(query)))
         count = hireables.length
