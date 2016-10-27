0ba3f80cc194f226520b9736695c4a20b4674afc
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-27 11:33:52 +0100

    Use remote url

--- app/services/authenticator.rb
+++ app/services/authenticator.rb
@@ -33,7 +33,7 @@ class Authenticator
       name: auth.info.name,
       uid: auth.uid,
       login: auth.info.nickname,
-      avatar: auth.extra.raw_info.avatar_url,
+      remote_avatar_url: auth.extra.raw_info.avatar_url,
       location: auth.extra.raw_info.location,
       provider: auth.provider,
       hireable: (auth.extra.raw_info.hireable.nil? ? false : true),
