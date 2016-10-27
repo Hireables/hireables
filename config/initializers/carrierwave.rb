b01b207fdb4aa7b4ea88f1af53fed5a198a8f2ee
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-27 11:35:33 +0100

    Fix carrierwave initialiser for test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,15 +11,17 @@ if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
   ImageUploader
 
   # use different dirs when testing
-  CarrierWave::Uploader::Base.descendants.each do |klass|
-    next if klass.anonymous?
-    klass.class_eval do
-      def cache_dir
-        "#{Rails.root}/spec/support/uploads/tmp"
-      end
+  if Rails.env.test?
+    CarrierWave::Uploader::Base.descendants.each do |klass|
+      next if klass.anonymous?
+      klass.class_eval do
+        def cache_dir
+          "#{Rails.root}/spec/support/uploads/tmp"
+        end
 
-      def store_dir
-        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        def store_dir
+          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        end
       end
     end
   end
