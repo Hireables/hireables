0c6312be9bcb6213c51494ee2980404198e2b89d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-28 16:31:28 +0100

    Fetch languages

--- app/workers/fetch_developer_worker.rb
+++ app/workers/fetch_developer_worker.rb
@@ -5,5 +5,9 @@ class FetchDeveloperWorker
   def perform(login)
     api = Github::Api.new
     api.fetch_developer(login)
+
+    FetchDeveloperLanguagesWorker.perform_async(
+      login
+    ) unless Rails.cache.exist?([login, 'languages'])
   end
 end
