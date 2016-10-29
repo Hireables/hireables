85421093fbc411360049833058ba293ba9238c3d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-29 16:20:36 +0100

    Change warden cookie key

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,12 +1,12 @@
 Warden::Manager.after_set_user do |user, auth, opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = user.id
+  auth.cookies.signed["#{scope}.login"] = user.login
   auth.cookies.signed["#{scope}.expires_at"] = 30.minutes.from_now
 end
 
 # Cleanup once logged out
 Warden::Manager.before_logout do |user, auth, opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = nil
+  auth.cookies.signed["#{scope}.login"] = nil
   auth.cookies.signed["#{scope}.expires_at"] = nil
 end
