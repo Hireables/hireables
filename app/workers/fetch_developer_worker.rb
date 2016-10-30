3228746967663eab4f58209eea90dd916b6bad56
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-10-30 05:42:14 +0000

    Remove language worker

--- app/workers/fetch_developer_worker.rb
+++ app/workers/fetch_developer_worker.rb
@@ -5,9 +5,5 @@ class FetchDeveloperWorker
   def perform(login, access_token)
     api = Github::Api.new(access_token)
     api.fetch_developer(login)
-
-    FetchDeveloperLanguagesWorker.perform_async(
-      login, access_token
-    ) unless Rails.cache.exist?([login, 'languages'])
   end
 end
