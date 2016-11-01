8f55f672a8bd20809ddac1dfdd6f064660cc7581
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-01 16:22:35 +0000

    Add current user to sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  respond_to :json, :html
-  include Tokenizeable
   protect_from_forgery with: :exception
+  respond_to :json, :html
+
   devise_group :user, contains: [:developer, :recruiter]
+
+  before_action :set_raven_context, if: :tracking?
+  before_action :store_current_location, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  include Tokenizeable
 
   protected
 
@@ -16,5 +21,39 @@ class ApplicationController < ActionController::Base
 
   def render_unauthorised
     render json: { errors: [ message: 'Unauthorised'] }, status: 401
+  end
+
+  private
+
+  def tracking?
+    Rails.env.production?
+  end
+
+  def set_raven_context
+    current_user_context = if user_signed_in?
+                             {
+                               id: current_user.id,
+                               email: current_user.email,
+                               type: current_user.class.name.downcase
+                             }
+                           else
+                             {}
+                           end
+
+    Raven.user_context(current_user_context)
+    Raven.extra_context(params: params.to_h, url: request.url)
+  end
+
+  # after login and logout path
+  def store_current_location
+    store_location_for(:user, request.referrer)
+  end
+
+  def after_sign_out_path_for(_resource)
+    request.referrer || root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 end
