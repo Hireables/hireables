3aa9149a27f9c5d558f7d26385563c10edb0c607
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-02 14:32:10 +0000

    Change recruiter to user

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ActionMailer::Base
   default to: ENV.fetch('SIGNUPS_EMAIL'),
           from: ENV.fetch('MAILER_EMAIL')
 
-  def new_recruiter_signup(user_type, user_id)
+  def new_user_signup(user_type, user_id)
     @user = user_types.fetch(user_type).find(user_id)
     mail(subject: "New #{user_type.downcase} signup!") do |format|
       format.text
