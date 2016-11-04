493ee64bb3fc7e51a4f45705dc6ddf9f4baffe8a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-04 07:54:30 +0000

    Update rack attack paths

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,37 @@
 class Rack::Attack
-  # Throttle all requests by IP (60rpm)
+  # Throttle all requests by IP (30rpm)
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', :limit => 3, :period => 1.second) do |req|
+  throttle('req/ip', limit: 150, period: 5.minutes) do |req|
     req.ip unless req.path.starts_with?('/assets')
   end
 
+  # Prevent Brute-Force Login Attacks
+  throttle('developers/auth/github', limit: 5, period: 5.minutes) do |req|
+    if req.path == '/developers/auth/github'
+      req.ip
+    end
+  end
+
+  # Prevent Brute-Force Login Attacks on devise routes
+  throttle('employers/sign_in', limit: 5, period: 5.minutes) do |req|
+    if req.path == '/employers/sign_in' && req.post?
+      req.ip
+    end
+  end
+
+  # Prevent Brute-Force Signup Attacks on devise routes
+  throttle('employers/sign_up', limit: 5, period: 5.minutes) do |req|
+    if req.path == '/employers/sign_up' && req.post?
+      req.ip
+    end
+  end
+
+  # Prevent Brute-Force forgot password Attacks on devise routes
+  throttle('employers/password/new', limit: 1, period: 5.minutes) do |req|
+    if req.path == '/employers/password/new' && req.post?
+      req.ip
+    end
+  end
 end
 
 # Configure app to use Rack::Attack in production
