6f76fe73c15038e17b3259785b6cc98ee6ccb9e1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-05 13:40:23 +0000

    Add scoped uniqueness at model level

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,6 +1,9 @@
 class Favourite < ApplicationRecord
+  validates_uniqueness_of :login, { scope: :employer_id }
+
   after_create :cache_favorited_login
   before_destroy :delete_cached_favorited_login
+
   belongs_to :employer
   belongs_to :developer
 
