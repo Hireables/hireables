cd28b460236e585176d8095398e728c3eb68a4c6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-05 05:08:33 +0000

    Setup redis instances with connection pool

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,9 @@
-REDIS = Redis.new(url: ENV['REDIS_URL'])
+Redis::Objects.redis = ConnectionPool.new(
+  size: ENV.fetch('REDIS_SIZE'),
+  timeout: 5
+) {
+  Redis.new(
+    url: ENV.fetch('DATABASE_REDIS_URL'),
+    namespace: "data_#{Rails.env.downcase}")
+  )
+}
