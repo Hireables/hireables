1af62f2e25cd1f8ed5cf9ca511a5da0131e9c129
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-06 04:36:49 +0000

    Create a class method to find and create connection from oauth

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,6 +3,11 @@ class Connection < ApplicationRecord
   validates_presence_of :uid, :provider, :access_token
   validates_uniqueness_of :uid, scope: :provider
 
+  def self.find_or_create_for_oauth(auth)
+    @connection ||= find_for_oauth(auth)
+    @connection.nil? ? create_from_oauth(auth) : @connection
+  end
+
   def self.find_for_oauth(auth)
     where(uid: auth.uid, provider: auth.provider).first
   end
@@ -11,8 +16,7 @@ class Connection < ApplicationRecord
    create!(
       uid: auth.uid,
       provider: auth.provider,
-      token: auth.credentials.token,
-      secret: auth.credentials.secret
+      access_token: auth.credentials.token,
     )
   end
 end
