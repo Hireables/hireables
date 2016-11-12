021f64776e3ed8074c2fa2a099d61e7af9332922
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-12 09:54:01 +0000

    Fix syntax

--- app/workers/fetch_developer_languages_worker.rb
+++ app/workers/fetch_developer_languages_worker.rb
@@ -2,11 +2,11 @@ class FetchDeveloperLanguagesWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'critical', retry: 5
 
+  # rubocop:disable Metrics/MethodLength
   def perform(login, access_token)
     api = Github::Api.new(access_token)
     languages = api.fetch_developer_languages(login)
 
-    # Update languages in a transaction block
     Developer.connection_pool.with_connection do
       developer = Developer.find_by_login(login)
       developer.update!(platforms: languages)
--- app/workers/import_connection_data_worker.rb
+++ app/workers/import_connection_data_worker.rb
@@ -2,6 +2,8 @@ class ImportConnectionDataWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'urgent', retry: 5
 
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def perform(connection_id)
     Developer.connection_pool.with_connection do
       connection = Connection.find(connection_id)
