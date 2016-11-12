915b2ec7ceb8f214a7d4635b38625f945df4ed90
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-12 15:10:45 +0000

    Update store accessor fields

--- app/models/import.rb
+++ app/models/import.rb
@@ -4,10 +4,11 @@ class Import < ApplicationRecord
 
   validates_presence_of :source_id, :data
   validates_uniqueness_of :source_id, scope: :connection_id
-  store_accessor :data, :html_url, :up_vote_count, :stargazers_count,
-                 :likeCount, :body, :summary, :description,
-                 :title, :name, :full_name, :link, :creation_date,
-                 :startDate, :publishedAt, :pushed_at
+
+  store_accessor :data, :location, :company, :thumbnails, :html_url,
+    :up_vote_count, :stargazers_count, :likeCount, :link, :isCurrent,
+    :homepage, :forks_count, :is_accepted, :comment_count, :score,
+    :body, :summary, :description, :title, :name, :full_name
 
   def self.by_source(source)
     where(source_name: source)
