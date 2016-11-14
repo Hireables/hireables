379e645ec537058d3071ff5205fec2994a9296c7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-14 14:17:50 +0000

    Add mutation for removing achivement from profile

---
+++ app/assets/javascripts/mutations/developer/removeAchievement.es6.js
@@ -0,0 +1,34 @@
+import Relay from 'react-relay';
+
+export default class extends Relay.Mutation {
+  getMutation() {
+    return Relay.QL`mutation{ RemoveAchievement }`;
+  }
+
+  getFatQuery() {
+    return Relay.QL`
+      fragment on RemoveAchievementPayload {
+        deletedId,
+        developer {
+          id,
+        }
+      }
+    `;
+  }
+
+  getConfigs() {
+    return [{
+      type: 'NODE_DELETE',
+      parentName: 'developer',
+      parentID: this.props.developerId,
+      connectionName: 'achievements',
+      deletedIDFieldName: 'deletedId',
+    }];
+  }
+
+  getVariables() {
+    return {
+      id: this.props.id,
+    };
+  }
+}
---
+++ app/graphql/resolvers/developers/remove_achievement_resolver.rb
@@ -0,0 +1,28 @@
+module Developers
+  class RemoveAchievementResolver
+    attr_reader :current_developer, :achievement, :params
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def initialize(_obj, inputs, ctx)
+      @current_developer = ctx[:current_developer]
+      @params = inputs
+      raise StandardError, 'Unauthorised' unless @current_developer.present?
+      @achievement = Schema.object_from_id(inputs['id'], ctx)
+      raise StandardError, 'Unauthorised' unless owner?
+    end
+
+    def call
+      {
+        developer: current_developer.reload,
+        deletedId: params['id']
+      }  if achievement.update(pinned: false)
+    end
+
+    def owner?
+      achievement.developer == current_developer
+    end
+  end
+end
