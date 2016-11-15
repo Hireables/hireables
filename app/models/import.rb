ebf519eae47618e983098def25f1cc80ba4f4b87
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-15 14:08:49 +0000

    Indent to same line

--- app/models/import.rb
+++ app/models/import.rb
@@ -6,9 +6,10 @@ class Import < ApplicationRecord
   validates_uniqueness_of :source_id, scope: :connection_id
 
   store_accessor :data, :location, :company, :thumbnails, :html_url,
-    :up_vote_count, :stargazers_count, :likeCount, :link, :isCurrent,
-    :homepage, :forks_count, :is_accepted, :viewCount, :comment_count, :score,
-    :body, :summary, :description, :title, :name, :full_name, :language
+                 :up_vote_count, :stargazers_count, :likeCount, :link,
+                 :isCurrent, :homepage, :forks_count, :is_accepted,
+                 :viewCount, :comment_count, :score, :body, :summary,
+                 :description, :title, :name, :full_name, :language
 
   def self.by_source(source)
     where(source_name: source)
