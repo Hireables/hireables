22718959c9021de4d2bf40ec35cf3c15e7034172
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-17 13:15:22 +0000

    Add meetup connector and component for rendering achievement

---
+++ app/assets/javascripts/components/developers/achievements/meetup.es6.jsx
@@ -0,0 +1,137 @@
+/* global $ */
+
+// Modules
+import React from 'react';
+import Relay from 'react-relay';
+import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import moment from 'moment';
+import Truncate from 'react-truncate';
+
+// Child Components icons
+import MeetupIcon from '../../shared/icons/meetup.es6';
+import sanitize from '../../../utils/sanitize.es6';
+
+const Meetup = (props) => {
+  const { achievement, remove } = props;
+  return (
+    <div className={`achievement ${achievement.source_name}`}>
+      <div className="achievement-block">
+        <div className={`achievement-point ${achievement.source_name}`}>
+          <MeetupIcon />
+        </div>
+        <div className="achievement-content">
+          <Card className="achievement-card full-width">
+            <div className="achievement-card-content">
+              <h2 className="intro">
+                <i className="icon material-icons">event</i>
+                Meetup
+              </h2>
+
+              {achievement.is_owner ?
+                <IconButton
+                  className="remove"
+                  tooltip="Remove"
+                  tooltipStyles={{ top: 25 }}
+                  onClick={event => remove(event, achievement)}
+                >
+                  <FontIcon className="material-icons">close</FontIcon>
+                </IconButton> : ''
+              }
+
+              <time className="date">
+                {
+                  moment
+                  .utc(new Date(achievement.created_at))
+                  .local()
+                  .format('MMMM Do YYYY')
+                }
+              </time>
+
+              <CardTitle
+                className="achievement-card-header"
+                title={
+                  <div className="title">
+                    <a
+                      href={achievement.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {achievement.name}
+                    </a>
+                  </div>
+                }
+              />
+
+              <Truncate
+                lines={5}
+                className="achievement-card-description"
+                ellipsis={
+                  <span>...
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={achievement.link}
+                    >
+                      Read more
+                    </a>
+                  </span>
+                }
+              >
+                <CardText
+                  dangerouslySetInnerHTML={{
+                    __html: sanitize(achievement.description).replace(/&nbsp;/g, ''),
+                  }}
+                />
+              </Truncate>
+
+              <CardActions className="meta">
+                <span className="badge">
+                  {`${achievement.yes_rsvp_count}`}
+                  <FontIcon
+                    color="#fff"
+                    className="material-icons"
+                    style={{
+                      fontSize: 20,
+                      verticalAlign: 'top',
+                      marginLeft: 5,
+                    }}
+                  >
+                    people
+                  </FontIcon>
+                </span>
+              </CardActions>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Meetup.propTypes = {
+  achievement: React.PropTypes.object,
+  remove: React.PropTypes.func,
+};
+
+const MeetupContainer = Relay.createContainer(Meetup, {
+  fragments: {
+    achievement: () => Relay.QL`
+      fragment on Import {
+        id,
+        name,
+        description,
+        source_name,
+        link,
+        is_owner,
+        yes_rsvp_count,
+        developer_id,
+        connection_id,
+        created_at,
+      }
+    `,
+  },
+});
+
+export default MeetupContainer;
---
+++ app/assets/javascripts/connectors/meetup.es6.js
@@ -0,0 +1,24 @@
+/* global window OAuth */
+
+export default class Meetup {
+  constructor() {
+    OAuth.initialize(window.OAUTH_KEY);
+  }
+
+  authenticate() {
+    return new Promise((resolve, reject) => {
+      OAuth.popup('meetup')
+      .done((result) => {
+        const expirationTime = new Date();
+        expirationTime.setSeconds(expirationTime.getSeconds() + result.expires_in);
+        result.me().then((data) => {
+          resolve({
+            access_token: result.access_token,
+            expires_at: expirationTime,
+            uid: data.raw.id
+          });
+        });
+      }).fail(err => (reject(err)));
+    });
+  }
+}
