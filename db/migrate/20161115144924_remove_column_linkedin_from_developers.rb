c6fc8061daa75c5a672edf4365c840eaf97795e7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-18 15:35:49 +0000

    Add back linkedin

--- db/migrate/20161115144924_remove_column_linkedin_from_developers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnLinkedinFromDevelopers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :developers, :linkedin, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115144924) do
+ActiveRecord::Schema.define(version: 20161115141410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161115144924) do
     t.string   "name",                default: "",    null: false
     t.string   "email",               default: "",    null: false
     t.text     "bio"
+    t.string   "linkedin",            default: ""
     t.string   "location",            default: ""
     t.string   "languages",           default: [],                 array: true
     t.boolean  "remote",              default: false
