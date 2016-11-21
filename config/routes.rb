065b1dc3269d861efd11df0bd16a4079548053a1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-21 14:08:41 +0000

    Don't debug assets in development

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#index'
-
   get '/cookies-policy', to: 'pages#cookies_policy'
   get '/privacy-policy', to: 'pages#privacy_policy'
+  get '/terms-of-service', to: 'pages#terms'
+  get '/upgrade-browser', to: 'pages#upgrade_browser'
 
   namespace :graphql do
     post '/', to: 'query#create'
