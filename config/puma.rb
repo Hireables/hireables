6db0bf3f43e83f01c0420827be93c1dd9d7083fa
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-23 10:32:37 +0000

    Add localtunnel config

--- config/puma.rb
+++ config/puma.rb
@@ -23,4 +23,19 @@ on_worker_boot do
   Que.mode = :async
 end
 
+unless Rails.env.production?
+  options = { addr: ENV['NGROK_PORT'] }
+  if File.file? '.ngrok'
+    options[:config] = '.ngrok'
+  elsif File.file? ENV['HOME'] + '/.ngrok'
+    options[:config] = ENV['HOME'] + '/.ngrok'
+  end
+
+  options[:inspect] = ENV['NGROK_INSPECT'] if ENV['NGROK_INSPECT']
+  puts '[NGROK] tunneling at ' + Ngrok::Tunnel.start(options)
+  unless ENV['NGROK_INSPECT'] == 'false'
+    puts '[NGROK] inspector web interface listening at http://127.0.0.1:4040'
+  end
+end
+
 plugin :tmp_restart
