5c026faf8ac0c0b992aeb824983387d4aecb486e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-24 07:33:15 +0000

    Monkey patch mailbox class to return type and id

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -5,3 +5,14 @@ Mailboxer.setup do |config|
   config.subject_max_length = 255
   config.body_max_length = 32000
 end
+
+Rails.application.config.to_prepare do
+  Mailboxer::Mailbox.class_eval do
+    attr_accessor :type, :id
+    def initialize(messageable, type='inbox')
+      @messageable = messageable
+      @type = type
+      @id = "#{messageable.class.name}-#{messageable.id}-#{type}"
+    end
+  end
+end
