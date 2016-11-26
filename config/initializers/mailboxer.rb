00c0363f74af3cc8f3f79526904320302dc3d799
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-26 16:49:22 +0000

    Add a find method to find mailbox by id

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -14,5 +14,25 @@ Rails.application.config.to_prepare do
       @type = type
       @id = "#{messageable.class.name}-#{messageable.id}-#{type}"
     end
+
+    def self.find(id)
+      id_parts = id.split('-')
+      messageable = Object.const_get(id_parts[0]).find(id_parts[1])
+      Mailboxer::Mailbox.new(messageable, id_parts[2])
+    end
+
+    def drafts
+      Mailboxer::Conversation.draft(messageable)
+    end
+  end
+
+  Mailboxer::Receipt.class_eval do
+    scope :draft, lambda { where(is_draft: true) }
+  end
+
+  Mailboxer::Conversation.class_eval do
+    scope :draft, lambda { |participant|
+      participant(participant).merge(Mailboxer::Receipt.draft)
+    }
   end
 end
