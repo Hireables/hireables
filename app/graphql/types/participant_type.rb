452b56ae33b6c8ea97d91ba06321298182398ca3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-28 13:38:28 +0000

    Add bio and other fields to participant

--- app/graphql/types/participant_type.rb
+++ app/graphql/types/participant_type.rb
@@ -7,9 +7,14 @@ ParticipantType = GraphQL::ObjectType.define do
   field :name, types.String, 'Participant name'
   field :avatar_url, types.String, 'Participant avatar'
   field :login, types.String, 'Participant username'
+  field :bio, types.String, 'The bio of this employer'
+  field :company, types.String, 'The company of this employer'
+  field :location, types.String, 'The location of this employer'
+
   field :database_id, types.Int, 'Participant database id' do
     resolve ->(obj, _args, _ctx) { obj.id }
   end
+
   field :type, types.String, 'Participant type' do
     resolve ->(obj, _args, _ctx) { obj.class.name.downcase }
   end
