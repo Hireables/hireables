f6f5a41468c88270b6cd83d0a35def0952e83263
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-28 16:28:17 +0000

    Change method name for clarity

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,9 @@
 class Search < ApplicationRecord
-  after_commit :cleanup_old_search, on: :create
+  after_commit :cleanup_old_searches, on: :create
   validates_presence_of :params, :employer_id
   belongs_to :employer
 
-  def cleanup_old_search
+  def cleanup_old_searches
     Search.order(id: :desc).offset(5).destroy_all
   end
 end
